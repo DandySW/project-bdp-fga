ccf4f7c870485b9058e9a25a211132dfd0c190a1
DandySW <dandysatrio123@gmail.com>
2022-08-21 13:00:11 +0700

    Berhasil menghubungkan Python dengan Javascript. Membuat list prediksi model.

--- Application/main.py
+++ Application/main.py
@@ -34,19 +34,7 @@ def prediction(user_input):
     predict = rfc.predict([array])
     predict = predict[0]
     print("Hasil prediksi: " + str(predict) + " - " + stres_level[predict])
-    # return(array)
+    return(int(predict))
 
 
 eel.start("index.html")
-
-# Predict This!
-# prediction([86.84,23.824,90.912,14.28,88.912,97.28,0.912,69.56])  #Medium High
-# prediction([54.08,18.816,94.816,8.816,93.224,82.04,5.816,57.04])  #Medium Low
-# prediction([80.72,22.192,90.096,12.24,88.096,95.24,0.096,65.48])  #Medium High
-# prediction([96.8,26.8,86,17.4,83.2,101,0,77])  #High
-# prediction([92.96,25.456,91.728,16.32,89.728,99.32,1.728,73.64])  #Medium High
-# prediction([78.24,21.824,93.824,11.824,91.824,94.12,4.736,64.56])  # Medium
-# prediction([97.184,27.184,86.48,17.592,83.776,101.48,0,77.96])  # High
-# prediction([99.456,29.456,89.32,18.728,87.184,104.32,0,83.64])  # High
-# prediction([99.232,29.232,89.04,18.616,86.848,104.04,0,83.08])  # High
-# prediction([57.84,19.568,95.568,9.568,94.352,83.92,6.568,58.92])  # Medium Low
